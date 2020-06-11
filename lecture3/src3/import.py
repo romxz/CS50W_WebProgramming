c2228a1f4ccd2def6b0ce00761161b5d824153b6
romxz <andreiromalv@gmail.com>
2020-06-11 16:37:35 -0400

    in:project1/

--- lecture3/src3/import.py
+++ lecture3/src3/import.py
@@ -8,13 +8,14 @@ engine = create_engine(os.getenv("DATABASE_URL"))
 db = scoped_session(sessionmaker(bind=engine))
 
 def main():
-    f = open("flights.csv")
-    reader = csv.reader(f)
-    for origin, destination, duration in reader:
-        db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
-                    {"origin": origin, "destination": destination, "duration": duration})
-        print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
-    db.commit()
+    with open("flights.csv") as f:
+        #f = open("flights.csv")
+        reader = csv.reader(f)
+        for origin, destination, duration in reader:
+            db.execute("INSERT INTO flights (origin, destination, duration) VALUES (:origin, :destination, :duration)",
+            {"origin": origin, "destination": destination, "duration": duration})
+            print(f"Added flight from {origin} to {destination} lasting {duration} minutes.")
+        db.commit()
 
 if __name__ == "__main__":
     main()
