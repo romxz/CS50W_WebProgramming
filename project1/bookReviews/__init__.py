012df7c81efa85cd40aee2c3dbe4165b19d09b33
romxz <andreiromalv@gmail.com>
2020-06-15 12:22:50 -0400

    fixed 'flask run' bug using __init__.py: source env from venv, FLASK_APP=bookReviews, then 'flask run' from parent folder

--- project1/bookReviews/__init__.py
+++ project1/bookReviews/__init__.py
@@ -5,17 +5,39 @@ from flask_sqlalchemy import SQLAlchemy
 # init SQLAlchemy so we can use it later in models
 db = SQLAlchemy()
 
-def create_app():
-    app = Flask(__name__)
+def create_app(test_config=None):
+    app = Flask(__name__, instance_relative_config=True)  # instance_relative_config=True
 
-    with open('./.skey') as f:
-        app.config['SECRET_KEY'] = f.readline().strip()
+    # Use test_config if provided, else build from local/
+    if test_config is None:
+        # Add secret key to app
+        if not os.path.isfile('bookReviews/local/.skey'):
+            msg = f'Please provide a key in "local/.skey"; {os.path.abspath(".")=}'
+            #print(msg)
+            raise Exception(msg)
+        else:
+            try:
+                with open('bookReviews/local/.skey') as f:
+                    app.config['SECRET_KEY'] = f.readline().strip()
+            except:
+                raise Exception('Please provide a key in "/local/.skey"')
+        
+        # Check for database environment variable
+        if not os.getenv("DATABASE_URL"): 
+            raise RuntimeError("DATABASE_URL is not set")
+        else: 
+            app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
+    else:
+        app.config.from_mapping(test_config)
+
+    # Check instance folder exists
+    try:
+        pass #os.makedirs(app.instance_path)
+    except OSError:
+        pass
     
-    # Check for environment variable
-    if not os.getenv("DATABASE_URL"):
-        raise RuntimeError("DATABASE_URL is not set")
-    else: app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv("DATABASE_URL")
-
+    '''
+    # Initialize SQLAlchemy with flask app
     db.init_app(app)
 
     # blueprint for auth routes in app
@@ -25,5 +47,10 @@ def create_app():
     # blueprint for non-auth routes of app
     from .main import main as main_blueprint
     app.register_blueprint(main_blueprint)
+    '''
 
+    @app.route('/hello')
+    def hello():
+        return 'Hello, World!'
+    
     return app
