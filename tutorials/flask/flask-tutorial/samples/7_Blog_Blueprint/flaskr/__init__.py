50328004ca75b140616c91af4addd64c2c80fb2c
romxz <andreiromalv@gmail.com>
2020-06-26 19:43:30 -0400

    out: '7_...: init /index routing on blog.py'; in: 7_...: init /create route on blog.py

--- tutorials/flask/flask-tutorial/samples/7_Blog_Blueprint/flaskr/__init__.py
+++ tutorials/flask/flask-tutorial/samples/7_Blog_Blueprint/flaskr/__init__.py
@@ -32,9 +32,13 @@ def create_app(test_config=None):
     from . import db
     db.init_app(app)
 
-    # Initialize route blueprints
+    # Initialize authorization blueprint
     from . import auth
     app.register_blueprint(auth.bp)
-
+    # Initialize main app feature (i.e. blog here) blueprint
+    from . import blog
+    app.register_blueprint(blog.bp)
+    # Make 'index' (@ blog app) root route
+    app.add_url_rule('/', endpoint='index')
 
     return app
