8f2f8ff959ba72f4d7fefd31d954b4504f55c67e
romxz <andreiromalv@gmail.com>
2020-06-26 21:02:01 -0400

    out: 'blog.update' and 'update.html.jinja'

--- tutorials/flask/flask-tutorial/samples/7_Blog_Blueprint/flaskr/blog.py
+++ tutorials/flask/flask-tutorial/samples/7_Blog_Blueprint/flaskr/blog.py
@@ -41,3 +41,44 @@ def create():
             return redirect(url_for('blog.index'))
         
     return render_template('blog/create.html.jinja')
+
+def get_post(id, check_author=True):
+    post = get_db().execute(
+        'SELECT p.id, title, body, created, author_id, username'
+        ' FROM posts p JOIN users u ON p.author_id = u.id'
+        ' WHERE p.id = ?', (id,)
+    ).fetchone()
+
+    if post is None:
+        abort(404, f"Post id {id} doesn't exist.")
+    
+    if check_author and post['author_id'] != g.user['id']:
+        abort(403)
+    
+    return post
+
+@bp.route('/<int:id>/update', methods=('GET', 'POST'))
+@login_required
+def update(id):
+    post = get_post(id)
+
+    if request.method == 'POST':
+        title = request.form['title']
+        body = request.form['body']
+        error = None
+
+        if not title:
+            error = 'Title is required'
+        
+        if error is not None:
+            flash(error)
+        else:
+            db = get_db()
+            db.execute(
+                'UPDATE posts SET title = ?, body = ?'
+                ' WHERE id = ?', (title, body, id)
+            )
+            db.commit()
+            return redirect(url_for('blog.index'))
+    
+    return render_template('blog/update.html', post=post)
