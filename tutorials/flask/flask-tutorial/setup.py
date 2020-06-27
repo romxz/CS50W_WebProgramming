1aecb4eb55f5d3a6eba88620050832743d72d4c2
romxz <andreiromalv@gmail.com>
2020-06-27 10:58:53 -0400

    wip: making installable. Setting FLASK_APP moved to .flaskenv and set automatically by dotenv for convenience

--- tutorials/flask/flask-tutorial/setup.py
+++ tutorials/flask/flask-tutorial/setup.py
@@ -1,2 +1,15 @@
 from dotenv import load_dotenv
 load_dotenv()
+
+from setuptools import find_packages, setup
+
+setup(
+    name='flaskr',
+    version='1.0.0',
+    packages=find_packages(),
+    include_package_data=True,
+    zip_safe=False,
+    install_requires=[
+        'flask',
+    ],
+)
