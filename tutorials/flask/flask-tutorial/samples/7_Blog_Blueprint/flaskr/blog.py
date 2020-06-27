bc78b2b1333247c166d6335f1e337bc18036f996
romxz <andreiromalv@gmail.com>
2020-06-26 21:05:53 -0400

    out: 'blog.delete' (deletebutton part of 'update.html.jinja')

--- tutorials/flask/flask-tutorial/samples/7_Blog_Blueprint/flaskr/blog.py
+++ tutorials/flask/flask-tutorial/samples/7_Blog_Blueprint/flaskr/blog.py
@@ -82,3 +82,13 @@ def update(id):
             return redirect(url_for('blog.index'))
     
     return render_template('blog/update.html', post=post)
+
+@db.route('/<int:id>/delete', methods=('POST',))
+@login_required
+def delete(id):
+    get_post(id)  # Existance check
+    db = get_db()
+    db.execute('DELETE FROM posts WHERE id = ?', (id,))
+    db.commit()
+    return redirect(url_for('blog.index'))
+
